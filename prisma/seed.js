1b0d5278785e0d6b4a1b2920cf62b19f0dc0de0f
Robert <agustinus2h@gmail.com>
2026-07-11 15:34:21 +0700

    up

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,23 +1,23 @@
 import "dotenv/config";
 
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "#root/generated/client/client.js"
+import { PrismaClient } from "#root/generated/client/client.js";
 import userInit from "./seeders/user/init.js";
-import masterInit from "./seeders/master/init.js"
-import inventoryInit from "./seeders/inventory/init.js"
-import employeeInit from "./seeders/employee/init.js"
-import hrmsInit from "./seeders/hrms/init.js"
+import masterInit from "./seeders/master/init.js";
+import inventoryInit from "./seeders/inventory/init.js";
+import employeeInit from "./seeders/employee/init.js";
+import hrmsInit from "./seeders/hrms/init.js";
 
 const adapter = new PrismaMariaDb({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "jmc",
-  connectionLimit: 5,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT ?? 5),
 });
 
-const prisma = new PrismaClient({adapter});
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   await userInit(prisma);
